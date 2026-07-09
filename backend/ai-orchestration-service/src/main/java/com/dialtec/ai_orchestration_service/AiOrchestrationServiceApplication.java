b062c1a5a00829f6fc27eb92cc18c9f6fefd4a35
medwalid <medwalidkh7@gmail.com>
2026-07-09 11:51:55 +0100

    All the spring boot basic structure for the microservices and for the mobile front has been added - Basic Entry

---
+++ backend/ai-orchestration-service/src/main/java/com/dialtec/ai_orchestration_service/AiOrchestrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dialtec.ai_orchestration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiOrchestrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiOrchestrationServiceApplication.class, args);
+	}
+
+}
